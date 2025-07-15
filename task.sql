44de204fd33deefc19f8509102e328a5de25f55c
Присяжніков <150026110+maximprysyazhnikov@users.noreply.github.com>
2025-07-16 00:04:42 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,20 +1,22 @@
 -- Use our database
 USE ShopDB;
 
-START TRANSACTION;
 
--- 1. Створюємо нове замовлення
+
 INSERT INTO Orders (CustomerID, Date)
+
 VALUES (1, '2023-01-01');
 
--- 2. Отримуємо останнє ID замовлення
+
 SET @last_order_id = LAST_INSERT_ID();
 
--- 3. Додаємо позицію замовлення (OrderItem)
+START TRANSACTION;
+
+
 INSERT INTO OrderItems (OrderID, ProductID, Count)
+
 VALUES (@last_order_id, 1, 1);
 
--- 4. Оновлюємо кількість на складі (WarehouseAmount)
 UPDATE Products
 SET WarehouseAmount = WarehouseAmount - 1
 WHERE ID = 1;
